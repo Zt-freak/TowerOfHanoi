028d1d14dd8585c0fb26c3ec89aad6d2f532c390
Zt-freak <bnkho98@hotmail.com>
2021-01-07 21:40:43 +0100

    initial commit

---
+++ TowerOfHanoi/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace TowerOfHanoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Stack1 = new System.Windows.Forms.ListBox();
+            this.Stack2 = new System.Windows.Forms.ListBox();
+            this.Stack3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Stack1
+            // 
+            this.Stack1.FormattingEnabled = true;
+            this.Stack1.Location = new System.Drawing.Point(13, 13);
+            this.Stack1.Name = "Stack1";
+            this.Stack1.Size = new System.Drawing.Size(120, 95);
+            this.Stack1.TabIndex = 0;
+            // 
+            // Stack2
+            // 
+            this.Stack2.FormattingEnabled = true;
+            this.Stack2.Location = new System.Drawing.Point(139, 12);
+            this.Stack2.Name = "Stack2";
+            this.Stack2.Size = new System.Drawing.Size(120, 95);
+            this.Stack2.TabIndex = 1;
+            // 
+            // Stack3
+            // 
+            this.Stack3.FormattingEnabled = true;
+            this.Stack3.Location = new System.Drawing.Point(265, 12);
+            this.Stack3.Name = "Stack3";
+            this.Stack3.Size = new System.Drawing.Size(120, 95);
+            this.Stack3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 114);
+            this.button1.Name = "1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Pick up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ClickAction);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 114);
+            this.button2.Name = "2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Pick up";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ClickAction);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 114);
+            this.button3.Name = "3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Pick up";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ClickAction);
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.Location = new System.Drawing.Point(13, 207);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(75, 23);
+            this.NewGameButton.TabIndex = 6;
+            this.NewGameButton.Text = "New Game";
+            this.NewGameButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 240);
+            this.Controls.Add(this.NewGameButton);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Stack3);
+            this.Controls.Add(this.Stack2);
+            this.Controls.Add(this.Stack1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Stack1;
+        private System.Windows.Forms.ListBox Stack2;
+        private System.Windows.Forms.ListBox Stack3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button NewGameButton;
+    }
+}
+
---
+++ TowerOfHanoi/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TowerOfHanoi
+{
+    public partial class Form1 : Form
+    {
+        private ListBox PickedUpStack;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Stack1.Items.Add(1);
+            this.Stack1.Items.Add(2);
+            this.Stack1.Items.Add(3);
+            this.Stack1.Items.Add(4);
+            this.Stack1.Items.Add(5);
+            this.Stack1.Items.Add(6);
+            this.Stack1.Items.Add(7);
+        }
+
+        private void ClickAction(object sender, EventArgs e)
+        {
+
+            Button clickedButton = (Button)sender;
+            switch (clickedButton.Text)
+            {
+                case "Pick up":
+                    this.PickUp(clickedButton);
+                    break;
+                case "Place down":
+                    this.PlaceDown(clickedButton);
+                    break;
+            }
+        }
+
+        private void PickUp(Button clickedButton)
+        {
+            switch (clickedButton.Name)
+            {
+                case "1":
+                    this.PickedUpStack = this.Stack1;
+                    break;
+                case "2":
+                    this.PickedUpStack = this.Stack2;
+                    break;
+                case "3":
+                    this.PickedUpStack = this.Stack3;
+                    break;
+            }
+            if (this.PickedUpStack.Items.Count > 0)
+            {
+                this.button1.Text = "Place down";
+                this.button2.Text = "Place down";
+                this.button3.Text = "Place down";
+            }
+            
+        }
+
+        private void PlaceDown(Button clickedButton)
+        {
+            ListBox PlacedDownStack = null;
+            switch (clickedButton.Name)
+            {
+                
+                case "1":
+                    PlacedDownStack = this.Stack1;
+                    break;
+                case "2":
+                    PlacedDownStack = this.Stack2;
+                    break;
+                case "3":
+                    PlacedDownStack = this.Stack3;
+                    break;
+            }
+            if (PlacedDownStack.Items.Count > 0)
+            {
+                if (
+                       Int32.Parse(this.PickedUpStack.Items[0].ToString()) <= Int32.Parse(PlacedDownStack.Items[0].ToString()) ||
+                       PlacedDownStack.Items.Count == 0
+                   )
+                {
+                    PlacedDownStack.Items.Insert(0, Int32.Parse(this.PickedUpStack.Items[0].ToString()));
+                    this.PickedUpStack.Items.RemoveAt(0);
+                    this.button1.Text = "Pick up";
+                    this.button2.Text = "Pick up";
+                    this.button3.Text = "Pick up";
+                }
+            }
+            else
+            {
+                PlacedDownStack.Items.Insert(0, Int32.Parse(this.PickedUpStack.Items[0].ToString()));
+                this.PickedUpStack.Items.RemoveAt(0);
+                this.button1.Text = "Pick up";
+                this.button2.Text = "Pick up";
+                this.button3.Text = "Pick up";
+            }
+        }
+    }
+}
